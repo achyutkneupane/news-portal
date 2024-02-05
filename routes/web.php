60ccc15e7de2717f5c2575b98259fd137c58edb0
Achyut Neupane <achyutkneupane@gmail.com>
2024-02-05 21:00:10 +0545

    Navbar set to primary

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,8 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
+
+Route::get('/', [\App\Http\Controllers\FrontController::class, 'landingPage'])->name('landing-page');
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
