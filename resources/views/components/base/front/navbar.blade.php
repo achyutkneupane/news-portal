20f239c9d99a740a6f38b994a426123872fbec47
Achyut Neupane <achyutkneupane@gmail.com>
2024-02-09 20:55:58 +0545

    Navbar page for front view created

---
+++ resources/views/components/base/front/navbar.blade.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-expand-lg bg-body border-bottom">
+    <div class="container">
+        <a class="navbar-brand" href="#">
+            <img src="{{ asset('/images/codeitnews.png') }}" alt="{{ config('app.name') }}" width="100">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact Us</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
--- resources/views/front-view.blade.php
+++ resources/views/front-view.blade.php
@@ -1,3 +1,4 @@
 <x-front-layout>
     <x-base.front.topbar />
+    <x-base.front.navbar />
 </x-front-layout>
