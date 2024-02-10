45e67772426b233aa3e6452576cbeabdc79d6c28
Achyut Neupane <achyutkneupane@gmail.com>
2024-02-10 21:06:35 +0545

    Top Article Section created with top-viewed article

--- resources/views/components/base/front/top-article.blade.php
+++ resources/views/components/base/front/top-article.blade.php
@@ -1,3 +1,24 @@
 <div>
-    Top Article
+    <div class="w-100 mb-2">
+        <img src="{{ asset('storage/images/'. $article->image) }}" class="w-100" />
+    </div>
+    <div>
+        <span class="bg-primary text-white p-1">
+            {{ $article->category->title }}
+        </span>
+    </div>
+    <div class="mt-2">
+        <h2 class="fw-bolder display-6">
+            {{ $article->title }}
+        </h2>
+    </div>
+    <div class="d-flex justify-content-start gap-2">
+        <div>
+            {{ $article->created_at->isoFormat('MMMM D, YYYY ') }}
+        </div>
+        <div>|</div>
+        <div>
+            {{ $article->views }} Views
+        </div>
+    </div>
 </div>
