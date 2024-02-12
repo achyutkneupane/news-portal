4c28eced79df28b3c6cd6eb70c98a70bf66bf5b6
Achyut Neupane <achyutkneupane@gmail.com>
2024-02-12 19:56:46 +0545

    Other Articles to list with description

--- app/View/Components/Base/Front/OtherArticles.php
+++ app/View/Components/Base/Front/OtherArticles.php
@@ -2,18 +2,20 @@
 
 namespace App\View\Components\Base\Front;
 
+use App\Models\Article;
 use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
 class OtherArticles extends Component
 {
+    public $articles;
     /**
      * Create a new component instance.
      */
     public function __construct()
     {
-        //
+        $this->articles = Article::latest()->offset(5)->limit(20)->get();
     }
 
     /**
--- resources/views/components/base/front/other-articles.blade.php
+++ resources/views/components/base/front/other-articles.blade.php
@@ -1,3 +1,5 @@
 <div class="container mt-5">
-    <x-base.front.article-with-description id="76" />
+    @foreach($articles as $article)
+        <x-base.front.article-with-description :id="$article->id" />
+    @endforeach
 </div>
